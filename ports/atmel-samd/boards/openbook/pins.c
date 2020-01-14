d88dd887775aae6596b8d6c47d8774a5482b3527
Joey Castillo <jose.castillo@gmail.com>
2020-01-14 13:43:08 -0500

    board.display is an epaper_display

--- ports/atmel-samd/boards/openbook/pins.c
+++ ports/atmel-samd/boards/openbook/pins.c
@@ -69,6 +69,6 @@ STATIC const mp_rom_map_elem_t board_global_dict_table[] = {
     { MP_ROM_QSTR(MP_QSTR_SPI), MP_ROM_PTR(&board_spi_obj) },
     { MP_ROM_QSTR(MP_QSTR_UART), MP_ROM_PTR(&board_uart_obj) },
 
-    { MP_ROM_QSTR(MP_QSTR_DISPLAY), MP_ROM_PTR(&displays[0].display)}
+    { MP_ROM_QSTR(MP_QSTR_DISPLAY), MP_ROM_PTR(&displays[0].epaper_display)}
 };
 MP_DEFINE_CONST_DICT(board_module_globals, board_global_dict_table);
